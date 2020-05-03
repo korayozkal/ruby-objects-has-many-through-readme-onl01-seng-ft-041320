ef243b96e02217316adf2079da07161b13e36ddf
Koray Ozkal <koryozkal@gmail.com>
2020-05-03 00:37:07 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -18,6 +18,12 @@ class Waiter
     Meal.new(self, customer, total, tip)
   end
   
+  def meals
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+  end
+end
+  
   def best_tipper
   best_tipped_meal = meals.max do |meal_a, meal_b|
     meal_a.tip <=> meal_b.tip
